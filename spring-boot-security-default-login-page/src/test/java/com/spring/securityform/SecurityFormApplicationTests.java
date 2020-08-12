d985a814c843d33c91812ba174060100edb02a03
Shivakumar <Shivakumar@DESKTOP-9PDBQK6>
2020-08-12 18:22:44 +0530

    default login page

---
+++ spring-boot-security-default-login-page/src/test/java/com/spring/securityform/SecurityFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.securityform;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
